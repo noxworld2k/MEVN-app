c25421f57d1d15c34e2ac943c261545d6ad92312
Patryk Kowalczyk <noxworl2k@gmail.com>
2023-03-21 20:28:18 +0100

    create crud for users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,38 +1,99 @@
 const express = require('express');
 const User = require('../schema/user.model');
 const bcrypt = require('bcrypt');
-
 const router = express.Router();
 
-// CRUD - Create, Read, Update, Delete
 
-// CREATE
 router.post('/', async (req, res) => {
-    // ... (kod związany z tworzeniem użytkownika)
+    const { firstName, lastName, email, password } = req.body;
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        });
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 // READ
 router.get('/', async (req, res) => {
-    // ... (kod związany z odczytywaniem wszystkich użytkowników)
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    // ... (kod związany z odczytywaniem pojedynczego użytkownika)
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 // UPDATE
 router.put('/:id', async (req, res) => {
-    // ... (kod związany z aktualizacją użytkownika)
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['firstName', 'lastName', 'email', 'password'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        updates.forEach((update) => user[update] = req.body[update]);
+        await user.save();
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 // DELETE
 router.delete('/:id', async (req, res) => {
-    // ... (kod związany z usuwaniem użytkownika)
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
-// Dodaj endpoint do weryfikacji hasła
+
 router.post('/verifyPassword', async (req, res) => {
-    // ... (kod związany z weryfikacją hasła)
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send('Invalid credentials');
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 module.exports = router;
